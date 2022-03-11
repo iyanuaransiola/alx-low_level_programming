7a7ed1d8b639ed2bb9ef94d3b3bba824291aac78
root <aransiolaiy@gmail.com>
2022-03-11 07:31:50 -0800

    Added inputs

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 The scripts in this repo are commands on intro to low level programming c
+Opening this directory unveils all special commands.  
---
+++ autorun.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git add .
+
+git commit -m "Added inputs"
+
+git push
+
+iyanuaransiola
+
+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
